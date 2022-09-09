10f35f99f6c3d6d16b6e8407f7353d63cf5ee2a8
Mark Mead <itsmarkmead@gmail.com>
2022-09-09 16:39:28 +0100

    Screen - Channels

--- packages/admin/resources/views/partials/forms/channel.blade.php
+++ packages/admin/resources/views/partials/forms/channel.blade.php
@@ -1,6 +1,6 @@
 <div class="space-y-4">
     <div class="overflow-hidden shadow sm:rounded-md">
-        <div class="flex-col px-4 py-5 space-y-4 bg-white sm:p-6">
+        <div class="flex-col px-4 py-5 space-y-4 bg-white border-white dark:bg-gray-800 dark:border-gray-700 sm:p-6">
             <x-hub::input.group :label="__('adminhub::inputs.name')"
                                 for="name"
                                 :error="$errors->first('channel.name')">
@@ -37,7 +37,7 @@
             </x-hub::input.group>
         </div>
 
-        <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
+        <div class="px-4 py-3 text-right bg-black/5 dark:bg-white/5 sm:px-6">
             <button type="submit"
                     class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 {{ __('adminhub::global.save') }}
@@ -46,18 +46,23 @@
     </div>
 
     @if ($channel->id && !$channel->getOriginal('default') && !$channel->wasRecentlyCreated)
-        <div class="bg-white border border-red-300 rounded shadow">
-            <header class="px-6 py-4 text-red-700 bg-white border-b border-red-300 rounded-t">
-                {{ __('adminhub::inputs.danger_zone.title') }}
+        <div class="overflow-hidden bg-white border border-red-300 rounded shadow dark:bg-gray-800 dark:border-red-700">
+            <header class="px-6 py-4 text-red-700 border-b border-red-300 dark:border-red-700 bg-red-500/5">
+                <strong>
+                    {{ __('adminhub::inputs.danger_zone.title') }}
+                </strong>
             </header>
 
             <div class="p-6 text-sm">
                 <div class="grid grid-cols-12 gap-4">
                     <div class="col-span-12 md:col-span-6">
-                        <strong>{{ __('adminhub::partials.forms.channel.delete_channel') }}</strong>
+                        <strong class="text-gray-900 dark:text-white">
+                            {{ __('adminhub::partials.forms.channel.delete_channel') }}
+                        </strong>
 
-                        <p class="text-xs text-gray-600">
-                            {{ __('adminhub::partials.forms.channel.channel_name_delete') }}</p>
+                        <p class="text-xs text-gray-600 dark:text-gray-300">
+                            {{ __('adminhub::partials.forms.channel.channel_name_delete') }}
+                        </p>
                     </div>
 
                     <div class="col-span-9 lg:col-span-4">
@@ -67,7 +72,9 @@
                     <div class="col-span-3 text-right lg:col-span-2">
                         <x-hub::button :disabled="!$this->canDelete"
                                        wire:click="delete"
-                                       type="button">{{ __('adminhub::global.delete') }}</x-hub::button>
+                                       type="button">
+                            {{ __('adminhub::global.delete') }}
+                        </x-hub::button>
                     </div>
                 </div>
             </div>
